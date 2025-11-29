990413d72dbf0ad7f5a1d4287258c514eb03c0ed
lalit <lalitpatil251092@gmail.com>
2025-11-29 15:29:49 +0530

    Set up Express server with a success message

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Jenkins Docker Project Successfully Running 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000...");
+});
 
